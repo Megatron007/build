2841351b77ce717c0eec078f65ff754336a3c417
Nathan Chancellor <natechancellor@gmail.com>
2016-11-16 11:40:51 -0700

    Core: clean up dumpvars
    I don't care about any of these things. Let's re-add the 2nd CPU info though.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/dumpvar.mk
+++ core/dumpvar.mk
@@ -1,7 +1,6 @@
 
 # List of variables we want to print in the build banner.
 print_build_config_vars := \
-  PLATFORM_VERSION_CODENAME \
   PLATFORM_VERSION \
   TARGET_PRODUCT \
   TARGET_BUILD_VARIANT \
@@ -14,13 +13,6 @@ print_build_config_vars := \
   TARGET_2ND_ARCH_VARIANT \
   TARGET_2ND_CPU_VARIANT \
   HOST_ARCH \
-  HOST_2ND_ARCH \
-  HOST_OS \
-  HOST_OS_EXTRA \
-  HOST_CROSS_OS \
-  HOST_CROSS_ARCH \
-  HOST_CROSS_2ND_ARCH \
-  HOST_BUILD_TYPE \
   BUILD_ID \
   OUT_DIR
 
