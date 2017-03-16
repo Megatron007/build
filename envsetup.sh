DirtyUnicorns/android_build

Use separate vendorsetup.sh for CAF devices [2/2]
Change-Id: Ie5061367b2236ba7e5935f09cc69abacfd1ffca7

envsetup.sh
@@ -463,7 +463,7 @@ function choosecombo()
     printconfig
 }
 
-# Clear this variable.  It will be built up again when the vendorsetup.sh
+# Clear this variable.  It will be built up again when the CAF vendorsetup.sh
 # files are included at the end of this file.
 unset LUNCH_MENU_CHOICES
 function add_lunch_combo()
@@ -517,7 +517,7 @@ function breakfast()
     DU_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
-    for f in `/bin/ls vendor/du/vendorsetup.sh 2> /dev/null`
+    for f in `/bin/ls vendor/du/caf-vendorsetup.sh 2> /dev/null`
         do
             echo "including $f"
             . $f
@@ -1704,9 +1704,9 @@ if [ "x$SHELL" != "x/bin/bash" ]; then
     esac
 fi
 
-# Execute the contents of any vendorsetup.sh files we can find.
-for f in `test -d device && find -L device -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
-         `test -d vendor && find -L vendor -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort`
+# Execute the contents of any caf vendorsetup.sh files we can find.
+for f in `test -d device && find -L device -maxdepth 4 -name 'caf-vendorsetup.sh' 2> /dev/null | sort` \
+         `test -d vendor && find -L vendor -maxdepth 4 -name 'caf-vendorsetup.sh' 2> /dev/null | sort`
 do
     echo "including $f"
     . $f

Megatron007/build

Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

envsetup.sh
@@ -510,7 +510,7 @@ function choosecombo()
     destroy_build_var_cache
 }
 
-# Clear this variable.  It will be built up again when the vendorsetup.sh
+# Clear this variable.  It will be built up again when the caf-vendorsetup.sh
 # files are included at the end of this file.
 unset LUNCH_MENU_CHOICES
 function add_lunch_combo()
@@ -585,7 +585,7 @@ function breakfast()
     target=$1
     CUSTOM_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
-    for f in `/bin/ls vendor/screwd/vendorsetup.sh 2> /dev/null`
+    for f in `/bin/ls vendor/screwd/caf-vendorsetup.sh 2> /dev/null`
         do
             echo "including $f"
             . $f
@@ -1750,9 +1750,9 @@ if [ "x$SHELL" != "x/bin/bash" ]; then
 fi
 
 # Execute the contents of any vendorsetup.sh files we can find.
-for f in `test -d device && find -L device -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
-         `test -d vendor && find -L vendor -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
-         `test -d product && find -L product -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort`
+for f in `test -d device && find -L device -maxdepth 4 -name 'caf-vendorsetup.sh' 2> /dev/null | sort` \
+         `test -d vendor && find -L vendor -maxdepth 4 -name 'caf-vendorsetup.sh' 2> /dev/null | sort` \
+         `test -d product && find -L product -maxdepth 4 -name 'caf-vendorsetup.sh' 2> /dev/null | sort`
 do
     echo "including $f"
     . $f